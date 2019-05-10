67e4165f90b9e8b60320fa4cc695c06f0aa9b72d
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 18:43:13 -0500

    Imported Wrapper component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Splashscreen from "./components/Splashscreen";
+import Wrapper from "./components/hoc/wrapper";
 import './App.css';
 
 function App() {
   return (
     <Router>
      <div>
+     <Wrapper>
       <Switch>
         <Route exact path="/"component={Splashscreen}/>
       </Switch>
+      </Wrapper>
       </div>
     </Router>
   );
