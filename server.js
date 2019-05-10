097769b067029b89c943b087281909471f06d74d
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 12:20:09 -0500

    Added body-parser

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
