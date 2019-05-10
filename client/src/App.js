3714e4ef6b3db9d15e0d8a143fa445de333bbdd6
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 18:35:57 -0500

    Imported BrowserRouter, Splashscreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Splashscreen from "./components/Splashscreen";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      MY APP
-    </div>
+    <Router>
+     <div>
+      <Switch>
+        <Route exact path="/"component={Splashscreen}/>
+      </Switch>
+      </div>
+    </Router>
   );
 }
 
