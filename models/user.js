a08f3360b67c920bccd44b0d4bed7f27a3ad862a
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 14:52:56 -0500

    Set up bcrypt and hashed out passwords using SALT

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const SALT_I = 10;
+
 const userSchema = mongoose.Schema({
     email: {
         type:String,
@@ -32,7 +35,33 @@ const userSchema = mongoose.Schema({
     token: {
         type:String
     }
-})
+});
+
+//Before we save our new User...we want to hash the password
+userSchema.pre('save' , function(next){
+    // References the userSchema, not the function
+    var user = this;
+
+    //If User modifies password then encrypt password
+    if(user.isModified('password')) {
+        bcrypt.genSalt(SALT_I, function(err, salt) {
+            // next kills whatever this entire line from 45 to 47 is doing and moves forward to the next command, which is working with saving outputting user response
+            if(err) return next(err)
+    
+            //hash uses the password of the hash(user.password)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) return next(err);
+                //users password (not the string) is equal to the hash
+                user.password = hash;
+                next();
+            });
+        });
+    }  else {
+        next()
+    }
+
+    
+});
 
 const User = mongoose.model('User', userSchema);
 module.exports = {User}
