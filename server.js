8cee225d66c2354c4d64487860d91796ebf12bbd
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 12:38:20 -0500

    Added static assets for heroku

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Remember-the-Vault-Project-3");
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  
+  
 
 
 const PORT = process.env.PORT || 3005;
