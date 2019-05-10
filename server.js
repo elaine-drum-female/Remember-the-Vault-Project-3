e5d97c8ee2864d25cada021835017b0ef36980aa
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 12:29:49 -0500

    Set up Mongoose to connect

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+const mongoose = require("mongoose");
+require('dotenv').config();
+mongoose.connect(process.env.MYDATABASE);
+
 const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
