7d4344574426d41e5c853f2383a91e9d30a57e4f
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 18:36:40 -0500

    Created two component Home and Splashscreen

---
+++ client/src/components/Splashscreen/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Splashscreen extends Component {
+    render() {
+        return (
+            <div>
+                Splashscreen
+            </div>
+        );
+    }
+}
+
+export default Splashscreen;
