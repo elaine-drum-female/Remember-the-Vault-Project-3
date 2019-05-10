1a2886bf813965fd2fafe7eb5761aa4d53836d5d
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 12:20:45 -0500

    Added port to listen to the server

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3005;
+
+app.listen(PORT, () => {
+    console.log(`Server running at PORT ${PORT}`);
+});
