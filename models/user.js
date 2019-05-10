49131eb5b37682fa3063726fc6c6efd4c6b9464e
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 13:04:14 -0500

    Set up the User schema

---
+++ models/user.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    email: {
+        type:String,
+        required: true,
+        unique: 1
+    },
+
+    password: {
+        type:String,
+        required: true,
+        minlength:5
+    },
+
+    name: {
+        type:String,
+        required:true,
+        maxlength:100
+    },
+
+    lastname: {
+        type:String,
+        required:true,
+        maxlength:100
+    },
+
+    role: {
+        type:Number,
+        default:0
+    },
+
+    token: {
+        type:String
+    }
+
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
