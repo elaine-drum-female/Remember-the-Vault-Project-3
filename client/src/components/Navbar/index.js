69f428ef47f7360f703573ae75edf291b9b19fb4
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 20:44:09 -0500

    Created the Navbar Component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./style.css";
+import { Link } from "react-router-dom";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+function Navbar() {
+  return (
+    <div className="nav-container">
+        <nav className="bground-color">
+          <ul className="nav">
+              <Link className="navbar-brand" to="/">Navbar</Link> 
+              <ul className="nav justify-content-end ml-auto">
+                <li className="nav-item">
+                  <i class="fas fa-user-circle"></i>
+                  <Link className="navbar-brand" to="/">My Account</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="navbar-brand" to="/">Sign Out</Link>
+                </li>
+              </ul>
+          </ul>
+        </nav>
+    </div>
+  );
+}
+
+export default Navbar;
