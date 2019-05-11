0236219205c65f9ac0ef9eff593d6a4d55fb9617
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 20:57:38 -0500

    Imported RegisterLogin Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/hoc/wrapper";
+import RegisterLogin from "./components/Register_Login";
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
      <Navbar />
      <Wrapper>
       <Switch>
+        <Route exact path="/register_login" component={RegisterLogin}/>
         <Route exact path="/"component={Home}/>
       </Switch>
       </Wrapper>
