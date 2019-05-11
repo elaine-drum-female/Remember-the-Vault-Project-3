61649155b4a7673a51e5c9630af120e3517a31d3
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-11 13:50:33 -0500

    Updated the last route link to registered_login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Wrapper from "./components/hoc/wrapper";
 import NewUser from "./components/NewRegister/";
@@ -16,7 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/register_login" component={RegisterLogin}/>
         <Route exact path="/register" component={NewUser} />
-        <Route exact path="/"component={Home}/>
+        <Route exact path="/signed_in" component={RegisterLogin} />
       </Switch>
       </Wrapper>
       </div>
