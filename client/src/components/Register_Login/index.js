ff1fd4f8721ae3504b2c930ff9b289321ed8be20
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 21:17:24 -0500

    Created the Register_Login component and code

--- client/src/components/Register_Login/index.js
+++ client/src/components/Register_Login/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
+
 const RegisterLogin = () => {
     return (
-        <div>
+        <div className="page_wrapper">
+            <div className="register_login_container">
+                <div className="left">
+                    <h1> New Customers</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada auctor lacus, quis ultricies magna fermentum ac. Quisque tristique dolor nec ultrices sodales. Sed sodales id magna maximus semper. Proin ultrices, lectus in imperdiet tincidunt, felis sapien rhoncus metus, nec sodales neque velit quis nisl. Fusce posuere fringilla iaculis. Maecenas.</p>
+                    
+                </div>
+                <div className="right">
+                    
+                </div>
+            </div>
             register
         </div>
     );
