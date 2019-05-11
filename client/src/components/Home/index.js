2351bf79f666e15160937e32259e5cef50a6dcf5
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-11 00:45:54 -0500

    Added link to register_login for demo purposes

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Home
+                <Link to="/register_login">Register Login</Link>
             </div>
         );
     }
 }
 
 export default Home;
+
