85989f24dc87308535de920a5ea7db78959de5b7
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-11 13:34:27 -0500

    Re-wrote it with importing Container, Row, Col

--- client/src/components/Register_Login/index.js
+++ client/src/components/Register_Login/index.js
@@ -1,28 +1,36 @@
 import React from 'react';
 import MyButton from "../utils/button";
 import Login from "../Register_Login/login";
+import Container from "../Container";
+import Row from "../Row";
+import Col from "../Col";
 import "./style.css";
 
 const RegisterLogin = () => {
     return (
-        <div className="page_wrapper">
-            <div className="register_login_container">
-                <div className="left">
-                    <h1> New Customers</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada auctor lacus, quis ultricies magna fermentum ac. Quisque tristique dolor nec ultrices sodales. Sed sodales id magna maximus semper. Proin ultrices, lectus in imperdiet tincidunt, felis sapien rhoncus metus, nec sodales neque velit quis nisl. Fusce posuere fringilla iaculis. Maecenas.</p>
-                    <MyButton 
+        <div className="jumbotron jumbotron-fluid mybackground">
+            <Container>
+                <Row>
+                    <Col size="lg-6">
+                        <span className="greeting">Hello!</span>
+                        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra
+                            attention to featured content or information.
+                        </p>
+                        <p>It uses utility classes for typography and spacing to space content out within the larger
+                        container.</p>
+                        <MyButton
                         type="default"
                         title="Register Here"
-                        linkTo="/register"
-                        
-                    />
-                </div>
-                <div className="right">
-                    <h2>Registered Customers</h2>
-                    <p>If you have an account please log in</p>
-                    <Login />
-                </div>
-            </div>
+                        linkTo="/register"    
+                        />
+                    </Col>
+                    <Col size="lg-6">
+                        <form>
+                            <Login />
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
