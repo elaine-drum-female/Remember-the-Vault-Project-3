6aaa7c5689dbadd1898ae5f8e89894718e8d20b4
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-10 23:05:23 -0500

    Updated the i class to className

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -11,7 +11,7 @@ function Navbar() {
               <Link className="navbar-brand" to="/">Remember the Vault</Link> 
               <ul className="nav justify-content-end ml-auto">
                 <li className="nav-item">
-                  <i class="fas fa-user-circle"></i>
+                  <i className="fas fa-user-circle"></i>
                   <Link className="navbar-brand" to="/">My Account</Link>
                 </li>
                 <li className="nav-item">
