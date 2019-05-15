81c2681d89e2c9d37070eed97323121c8be141ad
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-15 15:46:47 -0500

    Added the footer component

---
+++ client/src/components/Header_Footer/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                Testing for the footer
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
