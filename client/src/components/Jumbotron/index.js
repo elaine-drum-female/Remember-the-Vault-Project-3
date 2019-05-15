7bc6d41d987dbc8a245cdba725273f70fd78fe18
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-05-15 16:18:17 -0500

    Created the Jumbotron

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Jumbotron= () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+            <h1 className="display-3">Fluid jumbo heading</h1>
+            <p className="lead">Jumbo helper text</p>
+            <p>More info</p>
+            </div>
+        </div>
+    );
+};
+
+export default Jumbotron;
+
